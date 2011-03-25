6f2fd24eeba3e78b537f5f0def729b822025f543
TikiFlow Build Server <tiki_builder@butter.com.hk>
2011-03-25 08:36:37 +0000

    Build server auto document commit

--- api/classmap.js
+++ api/classmap.js
@@ -1,4 +1,4 @@
-YAHOO.env.classMap = {"TBind": "utils"};
+YAHOO.env.classMap = {"memoryMessageStore": "utils", "TBind": "utils", "Store": "utils", "Channel": "utils"};
 
 YAHOO.env.resolveClass = function(className) {
     var a=className.split('.'), ns=YAHOO.env.classMap;
